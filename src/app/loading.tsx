5bcf6169680ee91d27858a20a66e9a8726b6c3dd
Maria G <ruiined@gmail.com>
2024-01-14 01:00:39 +0000

    feat: gh actions workflow

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -8,8 +8,8 @@ export default function Loading() {
         stroke="currentColor"
         stroke-width="1.5"
         viewBox="0 0 24 24"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        stroke-lineCap="round"
+        stroke-lineJoin="round"
         xmlns="http://www.w3.org/2000/svg"
         className="animate-spin w-6 h-6 stroke-slate-500"
       >
