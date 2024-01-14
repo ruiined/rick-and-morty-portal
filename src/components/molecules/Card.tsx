cdcfb45db44792a5dfd4f7dd40945bf314d469a1
Maria G <ruiined@gmail.com>
2024-01-14 03:21:52 +0000

    perf: minor adjustments to the styling

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -1,6 +1,9 @@
 export const Card = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="md:w-48 rounded-lg shadow-lg flex flex-col justify-between">
+    <div
+      className="md:w-48 rounded-lg shadow-lg flex flex-col justify-between"
+      data-testid="card"
+    >
       {children}
     </div>
   );
--- src/components/molecules/ListItem.tsx
+++ src/components/molecules/ListItem.tsx
@@ -5,7 +5,7 @@ type ListItem = {
 
 export const ListItem = ({ label, value }: ListItem) => {
   return (
-    <p className="flex gap-1" data-testid="list-item">
+    <p data-testid="list-item">
       <span className="font-light text-slate-700">{label}:</span>
       <span className="tracking-wide truncate">{value}</span>
     </p>
